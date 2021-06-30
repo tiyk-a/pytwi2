120ec65ce9fa18d74893c531b76f45952865d181
Chiara <milejanaja@gmail.com>
2021-06-30 23:24:13 +0900

    Post method to Tweet

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import os, twitter
-from bottle import TEMPLATES, route, run
+from bottle import TEMPLATES, route, run, post, request
 from datetime import datetime
 
 @route("/")
@@ -21,5 +21,17 @@ def twitter(msg):
     )
     api.PostUpdate(msg)
     return "Tweeted"
+
+@route('/twi', method='POST')
+def twitter_post():
+    import twitter
+    api = twitter.Api(consumer_key=os.environ["CONSUMER_KEY"],
+        consumer_secret=os.environ["CONSUMER_SECRET"],
+        access_token_key=os.environ["ACCESS_TOKEN_KEY"],
+        access_token_secret=os.environ["ACCESS_TOKEN_SECRET"]
+    )
+    title = request.json['title']
+    api.PostUpdate(title)
+    return "Tweeted (POST)"
 
 run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
