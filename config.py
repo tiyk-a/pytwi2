59cf043f9fa25d6ff4da6c6aecf38b858cb0cdfe
Chiara <milejanaja@gmail.com>
2021-07-03 15:14:49 +0900

    Team division created

---
+++ config.py
@@ -0,0 +1,10 @@
+app_name = "pytwi2" # 上で作成したアプリケーション名
+consumer_key = "J97obYNcAFauZIndYTa6h7Lwm" # アプリケーション管理画面のConsumer API Keys > API key
+consumer_secret = "yc1k2zis9E53vPv5wlVrSnTsML2X9Z2b2eU8mxI5sSR0pvGRYZ"  # アプリケーション管理画面のConsumer API Keys > API secret key
+token = "1409384870745313286-9yPpw6az93vCyGEK1JJGX0DgNbm7SX"
+token_secret = "3GZqtD6NoWtwLMr43gKVYtcTqVjMfeI3Ig3pRK4Y0RURE"
+
+snowman_consumer_key="wVsD8xCkpMXIAdQ8dqidzVKWv"
+snowman_consumer_secret = "DGe3lGA82F9MjRMvzEvrnM2ALWOudV56CeDdNfOq3lBmQU7dHs"  # アプリケーション管理画面のConsumer API Keys > API secret key
+snowman_token = "1409169347277332481-W8q1hKoB3ojqRRbJU6HDbVzGLSyq9O"
+snowman_token_secret = "4OOl0DC9OvDsttHdf13VcmhzQmysrPicSwIuk3LNrLkvs"
--- index.py
+++ index.py
@@ -3,6 +3,8 @@ import os, twitter, urllib.parse
 from bottle import TEMPLATES, route, run, post, request, HTTPResponse
 from datetime import datetime
 
+from twitter import *
+from config import *
 @route("/")
 def hello_world():
     return "Hello"
@@ -11,28 +13,53 @@ def hello_world():
 def hello():
     return "Hola"
 
-@route('/twi/<msg>')
-def twitter(msg):
-    import twitter
-    api = twitter.Api(consumer_key=os.environ["CONSUMER_KEY"],
-        consumer_secret=os.environ["CONSUMER_SECRET"],
-        access_token_key=os.environ["ACCESS_TOKEN_KEY"],
-        access_token_secret=os.environ["ACCESS_TOKEN_SECRET"]
-    )
-    api.PostUpdate(msg)
-    return "Tweeted"
+# @route('/twi/<msg>')
+# def twitter(msg):
+#     import twitter
+#     api = twitter.Api(consumer_key=os.environ["CONSUMER_KEY"],
+#         consumer_secret=os.environ["CONSUMER_SECRET"],
+#         access_token_key=os.environ["ACCESS_TOKEN_KEY"],
+#         access_token_secret=os.environ["ACCESS_TOKEN_SECRET"]
+#     )
+#     api.PostUpdate(msg)
+#     return "Tweeted"
 
 @route('/twi', method='POST')
 def twitter_post():
-    import twitter
-    api = twitter.Api(consumer_key=os.environ["CONSUMER_KEY"],
-        consumer_secret=os.environ["CONSUMER_SECRET"],
-        access_token_key=os.environ["ACCESS_TOKEN_KEY"],
-        access_token_secret=os.environ["ACCESS_TOKEN_SECRET"]
+    # import twitter
+    # api = twitter.Api(consumer_key=os.environ["CONSUMER_KEY"],
+    #     consumer_secret=os.environ["CONSUMER_SECRET"],
+    #     access_token_key=os.environ["ACCESS_TOKEN_KEY"],
+    #     access_token_secret=os.environ["ACCESS_TOKEN_SECRET"]
+    # )
+    snowman = Twitter(
+        auth=OAuth(
+            snowman_token, # token（config.txtの1行目）
+            snowman_token_secret, # token_secret（config.txtの2行目）
+            snowman_consumer_key,
+            snowman_consumer_secret,
+        )
     )
+    t = Twitter(
+        auth=OAuth(
+            token, # token（config.txtの1行目）
+            token_secret, # token_secret（config.txtの2行目）
+            consumer_key,
+            consumer_secret,
+        )
+    )
+    teamId = request.json['teamId']
     title = request.json['title']
     try:
-        api.PostUpdate(urllib.parse.unquote(title, encoding='shift-jis'))
+        if teamId == 0:
+            t.statuses.update(status=urllib.parse.unquote(title + "ue", encoding='shift-jis'))
+            snowman.statuses.update(status=urllib.parse.unquote(title + "shita", encoding='shift-jis'))
+        # elif teamId == 1:
+        #     t.statuses.update(status=urllib.parse.unquote(title + "ue", encoding='shift-jis'))
+        elif teamId == 6:
+            snowman.statuses.update(status=urllib.parse.unquote(title + "ue", encoding='shift-jis'))
+        else:
+            t.statuses.update(status=urllib.parse.unquote(title + "ue", encoding='shift-jis'))
     except twitter.TwitterError as e:
         print(e)
         return HTTPResponse(status=201)
